ee63a5fe69f47d7ae5d9fa59d39fef73d996b1e9
igor <denys.s@sam-solutions.pro>
2022-12-07 19:34:51 +0200

    fix/form-script - refactor  code

--- php/send.php
+++ php/send.php
@@ -1,38 +1,13 @@
 <?php
-$name1 = $_POST['name1'];
-$tel2 = $_POST['tel2'];
-$date3 = $_POST['date3'];
-$text26 = $_POST['text26'];
-$number5 = $_POST['number5'];
 
-
-$name1 = htmlspecialchars($name1);
-$tel2 = htmlspecialchars($tel2);
-$date3 = htmlspecialchars($date3);
-$text26 = htmlspecialchars($text26);
-$number5 = htmlspecialchars($number5);
-
-
-
-
-$name1 = urldecode($name1);
-$tel2 = urldecode($tel2);
-$date3 = urldecode($date3);
-$text26 = urldecode($text26);
-$number5 = urldecode($number5);
-
-
-
-
-$name1 = trim($name1);
-$tel2 = trim($tel2);
-$date3 = trim($date3);
-$text26 = trim($text26);
-$number5 = trim($number5);
-
+$name1 = trim(urldecode(htmlspecialchars($_POST['name1'])));
+$tel2 = trim(urldecode(htmlspecialchars($_POST['tel2'])));
+$date3 = trim(urldecode(htmlspecialchars($_POST['date3'])));
+$text26 = trim(urldecode(htmlspecialchars($_POST['text26'])));
+$number5 = trim(urldecode(htmlspecialchars($_POST['number5'])));
 
 $filename = 'date.txt';
-$somecontent = "$name1, $tel2, $date3, $text26, $number5\n";
+$somecontent = "$name1, $tel2, $date3, $text26, $number5";
 
 if (is_writable($filename)) {
 
